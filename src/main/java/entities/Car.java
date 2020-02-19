4ec6b3eef29aa35049ab8baf8a489455e9c327c9
cprejler <prejler93@gmail.com>
2020-02-19 11:50:13 +0100

    Lavet Car endpoint og testet /all /count

--- src/main/java/entities/Car.java
+++ src/main/java/entities/Car.java
@@ -10,12 +10,19 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 
 /**
  *
  * @author casper
  */
+
 @Entity
+@NamedQueries({
+    @NamedQuery(name = "Cars.deleteAllRows", query = "DELETE from Car"),
+    @NamedQuery(name = "Cars.getAll", query = "SELECT c from Car c")
+})
 public class Car implements Serializable {
 
     private static final long serialVersionUID = 1L;
---
+++ src/main/java/rest/CarResource.java
@@ -0,0 +1,53 @@
+package rest;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import facades.CarFacade;
+import utils.EMF_Creator;
+import javax.persistence.EntityManagerFactory;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+//Todo Remove or change relevant parts before ACTUAL use
+@Path("cars")
+public class CarResource {
+
+    private static final EntityManagerFactory EMF = EMF_Creator.createEntityManagerFactory(
+                "pu",
+                "jdbc:mysql://localhost:3307/ca1",
+                "dev",
+                "ax2",
+                EMF_Creator.Strategy.CREATE);
+    
+    //An alternative way to get the EntityManagerFactory, whithout having to type the details all over the code
+    //EMF = EMF_Creator.createEntityManagerFactory(DbSelector.DEV, Strategy.CREATE);
+    
+    private static final CarFacade FACADE =  CarFacade.getFacadeExample(EMF);
+    private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
+            
+    @GET
+    @Produces({MediaType.APPLICATION_JSON})
+    public String demo() {
+        return "{\"msg\":\"Hello World\"}";
+    }
+    
+    @GET
+    @Path("all")
+    @Produces({MediaType.APPLICATION_JSON})
+    public String getAllCars(){
+        return GSON.toJson(FACADE.getAllCars());
+    }
+    
+    @GET
+    @Path("count")
+    @Produces({MediaType.APPLICATION_JSON}) 
+    public String getCarCount () {
+        long count = FACADE.getCarCount(); 
+        
+        return "{\"count\":" + count + "}";
+    }
+}
+ 
+
