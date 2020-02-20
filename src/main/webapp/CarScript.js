dc43b135c81f4a4b240f5cb3629fa5127f72a0e9
ATThorsen <thorsen.aske@gmail.com>
2020-02-20 10:27:01 +0100

    push, så andre kan arbejde videre

--- src/main/webapp/CarScript.js
+++ src/main/webapp/CarScript.js
@@ -1,22 +1,28 @@
 let AllCars = "http://localhost:8080/CA1/api/cars/all";
 
-HTMLPrint();
-myfetch();
-let carList = [];
+    let carList = [];
+    afetch();
 
-function myfetch(){
-    fetch(AllCars)
-.then(res => res.json()) //in flow1, just do it
-.then(data => { 
-        carList = data.forEach(e =>{
-                    return Object.values(data[e]) + Object.getOwnPropertyNames(data[e]);
-    })
-    console.log(carList)
-    }
+    HTMLPrint();
+
+
+
+
+function afetch(){
+fetch(AllCars)
+  .then(res => res.json())
+  .then(data => {
+
+                carList = (data)
+                console.log(carList)
+
+  }
+          
 )
+
 }
 
-
+console.log(carList)
 
 function HTMLPrint() {
 fetch(AllCars)
@@ -31,7 +37,6 @@ fetch(AllCars)
                 return "<th>" + a + "</th>"
             }).join("") +
             "</tr>";
-    console.log(headerString);
     
     
     let htmlRows = "<tr>"
@@ -45,6 +50,26 @@ fetch(AllCars)
     document.getElementById("div1").innerHTML = "<table border='1'>" + headerString + htmlRows; 
    })
 }    
+
+function HTMLPrint1(list) {
+    let headers = (Object.getOwnPropertyNames(list[0]));
+    let headerString = "<tr>" +
+            headers.map(function (a) {
+                return "<th>" + a + "</th>"
+            }).join("") +
+            "</tr>";
+    console.log(headerString);
+    let htmlRows = "<tr>"
+        list.forEach(e => {
+        let temp = Object.values(e).map(function (a) {
+            return "<td>" + a + "</td>";
+        }).join("") + "</tr>";
+        htmlRows += temp;
+    })
+    console.log(htmlRows);
+    document.getElementById("div1").innerHTML = "<table border='1'>" + headerString + htmlRows;
+}
+
 
 function filter(evt) {
     evt.preventDefault();
