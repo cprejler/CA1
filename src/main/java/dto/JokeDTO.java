866b5224c7c7f053ffc1336efd5eaf3c5a306f23
Plebbz <jens.ohlendorff@gmail.com>
2020-02-20 13:01:53 +0100

    joke javascript og html done

--- src/main/java/dto/JokeDTO.java
+++ src/main/java/dto/JokeDTO.java
@@ -14,20 +14,18 @@ import entities.Joke;
 public class JokeDTO {
     private String jokeText;
     private String type;
-    private String reference;
     private long jokeID;
 
-    public JokeDTO(String jokeText, String type, String reference, long jokeID) {
+    public JokeDTO(String jokeText, String type, long jokeID) {
         this.jokeText = jokeText;
         this.type = type;
-        this.reference = reference;
         this.jokeID = jokeID;
     }
 
     public JokeDTO(Joke joke) {
         this.jokeText = joke.getJokeText();
         this.type = joke.getType();
-        this.reference = joke.getReference();
+        this.jokeID = joke.getId();
 
     }
 }
