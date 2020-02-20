1e6a5548dc851241b9b6b4474b70e4d34d5a57d8
Malthorn1 <mikkelskott@gmail.com>
2020-02-20 12:42:51 +0100

    rettet findByID til at køre DTO

--- src/test/java/facades/MemberFacadeTest.java
+++ src/test/java/facades/MemberFacadeTest.java
@@ -1,5 +1,6 @@
 package facades;
 
+import dto.MemberDTO;
 import utils.EMF_Creator;
 import entities.Member;
 import java.util.List;
@@ -86,15 +87,13 @@ public class MemberFacadeTest {
     
     //@Test
     public void testFindByID(){
-        List<Member> member = facade.findByID(0);
+        List<MemberDTO> member = facade.findByID(0);
         String name = member.get(0).getName();
         String github = member.get(0).getGithub();
-        String email = member.get(0).getEmail();
         String studentID = member.get(0).getStudentID();
         
         assertTrue(facade.findByID(1).get(0).getName().contains(name));
         assertTrue(facade.findByID(1).get(0).getGithub().contains(github));
-        assertTrue(facade.findByID(1).get(0).getEmail().contains(email));
         assertTrue(facade.findByID(1).get(0).getStudentID().contains(studentID));
     }
     
